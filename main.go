5552852e184492c25c016186637dd32d543258ca
Erwin Wiesensarter <erwinwiesensarter@web.de>
2019-11-28 14:18:51 +0000

    ADD: minor

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"flag"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
 	"net/http/httputil"
@@ -38,7 +39,7 @@ func (b *Backend) SetAlive(alive bool) {
 	b.mux.Unlock()
 }
 
-// IsAlive returns true when caster is alive
+// IsAlive returns true when backend is alive
 func (b *Backend) IsAlive() (alive bool) {
 	b.mux.RLock()
 	alive = b.Alive
@@ -53,8 +54,8 @@ type BackendPool struct {
 }
 
 // AddBackend to the server pool
-func (bp *BackendPool) AddBackend(caster *Backend) {
-	bp.backends = append(bp.backends, caster)
+func (bp *BackendPool) AddBackend(backend *Backend) {
+	bp.backends = append(bp.backends, backend)
 }
 
 // NextIndex atomically increase the counter and return an index
@@ -62,7 +63,7 @@ func (bp *BackendPool) NextIndex() int {
 	return int(atomic.AddUint64(&bp.current, uint64(1)) % uint64(len(bp.backends)))
 }
 
-// MarkBackendStatus changes a status of a caster
+// MarkBackendStatus changes a status of a backend
 func (bp *BackendPool) MarkBackendStatus(casterURL *url.URL, alive bool) {
 	for _, b := range bp.backends {
 		if b.URL.String() == casterURL.String() {
@@ -74,12 +75,12 @@ func (bp *BackendPool) MarkBackendStatus(casterURL *url.URL, alive bool) {
 
 // GetNextPeer returns next active peer to take a connection
 func (bp *BackendPool) GetNextPeer() *Backend {
-	// loop entire backends to find out an Alive caster
+	// loop entire backends to find out an Alive backend
 	next := bp.NextIndex()
 	l := len(bp.backends) + next // start from next and move a full cycle
 	for i := next; i < l; i++ {
 		idx := i % len(bp.backends)     // take an index by modding
-		if bp.backends[idx].IsAlive() { // if we have an alive caster, use it and store if its not the original one
+		if bp.backends[idx].IsAlive() { // if we have an alive backend, use it and store if its not the original one
 			if i != next {
 				atomic.StoreUint64(&bp.current, uint64(idx))
 			}
@@ -131,6 +132,16 @@ func lb(w http.ResponseWriter, req *http.Request) {
 	if req.URL.Path == "/" {
 		log.Println("request sourcetable")
 		// TODO: build common sourcetable
+
+		// w.Header().Set("Trailer", "AtEnd1, AtEnd2")
+		// w.Header().Add("Trailer", "AtEnd3")
+		w.Header().Set("Ntrip-Version", "Ntrip/2.0")
+		w.Header().Set("Server", "NTRIP BKG Proxy 0.1/2.0")
+		w.Header().Set("Content-Type", "gnss/sourcetable")
+		w.Header().Set("Connection", "close")
+		w.WriteHeader(http.StatusOK)
+		io.WriteString(w, "Here comes the combined sourcetable...\n")
+		return
 	}
 
 	peer := backendPool.GetNextPeer()
@@ -148,7 +159,7 @@ func lb(w http.ResponseWriter, req *http.Request) {
 	http.Error(w, "Service not available", http.StatusServiceUnavailable)
 }
 
-// isAlive checks whether a caster is Alive by establishing a TCP connection
+// isAlive checks whether a backend is Alive by establishing a TCP connection
 /* func isCasterAlive(u *url.URL) bool {
 	timeout := 2 * time.Second
 	conn, err := net.DialTimeout("tcp", u.Host, timeout)
@@ -206,7 +217,7 @@ func main() {
 				return
 			}
 
-			// after 3 retries, mark this caster as down
+			// after 3 retries, mark this backend as down
 			backendPool.MarkBackendStatus(serverURL, false)
 
 			// if the same request routing for few attempts with different backends, increase the count
